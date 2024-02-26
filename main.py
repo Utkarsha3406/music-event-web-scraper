7c2727934087c67724424b383273284048c07efd
Utkarsha3406 <up3406@srmist.edu.in>
2024-02-26 20:23:45 +0530

    storing new tours in a file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import selectorlib as selectorlib
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
 def scrape(url):
     """Scrape the page source from URL"""
     response = requests.get(url, headers=HEADERS)
@@ -15,7 +17,26 @@ def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
+
+
+def send_email():
+    print("Email was sent")
+
+def store(extracted):
+    with open("data.txt","a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__=="__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
